dd2f12ed879c278d0468606cc9432b89cc73f44b
Dr. Appointment <drappointment.hackathon@gmail.com>
2014-08-21 22:42:49 +0530

    PUSH

--- index.js
+++ index.js
@@ -33,6 +33,11 @@ app.get('/appointment', function(req, res){
 	res.send(json_data);
 });
 
+//app.get('/servicename', function(req, res){}); ---should accept parameters
+//app.post('/servicename', function(req, res){}); ---should accept parameters
+
+//Google Calendar - Node package installation
+//PUSH notification
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
@@ -44,4 +49,33 @@ app.get('/', function(request, response) {
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
+
+function AndroidPush(){
+	var gcm = require('node-gcm');
+	var message = new gcm.Message();
+
+	//API Server Key
+	var sender = new gcm.Sender('AIzaSyCQyahIKMdL8DrdAwhG9bMcD3NXuu-c94g');
+	var registrationIds = [];
+	message.addData('message',"Push message test....");
+	message.addData('title','Dr. Appointment' );
+	//message.addData('msgcnt','1'); // Shows up in the notification in the status bar
+	message.addData('soundname','beep.wav'); //Sound to play upon notification receipt - put in the www folder in app
+	//message.collapseKey = 'demo';
+	//message.delayWhileIdle = true; //Default is false
+	message.timeToLive = 3000;// Duration in seconds to hold in GCM and retry before timing out. Default 4 weeks (2,419,200 seconds) if not specified.
+
+	// At least one reg id required
+	registrationIds.push('APA91bF-wA3Zz0X3VKx7nm0NniG52MeSrOBVU8jmGSu2N1tdy4iAHqmGWNRK7KdRHcRMF4luWBb-47W4AVnDZBAhTe_udoVR7Q7Wl2LberWR2n9YZc7dvcl4_Y9XRWSp6R2qSMk9qvpTwqU8HPzaXa966OrFGJ7hFQ');
+
+	/**
+	* Parameters: message-literal, registrationIds-array, No. of retries, callback-function
+	*/
+	sender.send(message, registrationIds, 0, function (result) {
+		console.log(result);
+	});
+}
+app.get('/push', function(req, res){
+	res.send('SUCCESS');
+});
 
