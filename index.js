80ec122d98f00fa40b03c154609a56a11d13d592
Dr. Appointment <drappointment.hackathon@gmail.com>
2014-08-21 22:04:24 +0530

    Mongo Lab

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 var express = require('express')
 var app = express();
 
+var mongo = require('mongodb');
+
+var mongoUri = process.env.MONGOLAB_URI ||
+  process.env.MONGOHQ_URL ||
+  'mongodb://localhost/mydb';
+
+//'mongodb://drappointment_admin:WallaWalla123@ds055709.mongolab.com:55709/appdb';
+
+mongo.Db.connect(mongoUri, function (err, db) {
+  db.collection('mydocs', function(er, collection) {
+    collection.insert({'mykey': 'myvalue'}, {safe: true}, function(er,rs) {
+    });
+  });
+});
+
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
@@ -11,3 +26,4 @@ app.get('/', function(request, response) {
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
+
