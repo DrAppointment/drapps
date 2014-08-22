7e50ca55228dbb4a4e2f4c368e537c23d1732ff2
Dr. Appointment <drappointment.hackathon@gmail.com>
2014-08-22 11:51:03 +0530

    Push service modification

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 var express = require('express')
+	, pushService = require('./pushService').PushService
+	//, routes = require('./routes')
+	//, user = require('./routes/user')
+	//, http = require('http')
+	//, path = require('path')
+	, mongo = require('mongodb');
+
 var app = express();
 
-var mongo = require('mongodb');
-
-var mongoUri = process.env.MONGOLAB_URI ||
-  process.env.MONGOHQ_URL ||
-  'mongodb://localhost/mydb';
-
-//'mongodb://drappointment_admin:WallaWalla123@ds055709.mongolab.com:55709/appdb';
+var mongoUri = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || 'mongodb://localhost/mydb';
 
 mongo.Db.connect(mongoUri, function (err, db) {
-  db.collection('mydocs', function(er, collection) {
-    collection.insert({'mykey': 'myvalue'}, {safe: true}, function(er,rs) {
-    });
-  });
+	db.collection('mydocs', function(er, collection) {
+		collection.insert({'mykey': 'myvalue'}, {safe: true}, function(er,rs) {
+		});
+	});
 });
 
 app.get('/appointment', function(req, res){
@@ -37,46 +38,27 @@ app.get('/appointment', function(req, res){
 //app.post('/servicename', function(req, res){}); ---should accept parameters
 
 //Google Calendar - Node package installation
-//PUSH notification
+
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello World !!! <br/> v1.2');
+  response.send('Dr. Appointment !!! <br/> v1.3');
 })
 
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'))
 })
 
-function AndroidPush(){
-	var gcm = require('node-gcm');
-	var message = new gcm.Message();
-
-	//API Server Key
-	var sender = new gcm.Sender('AIzaSyCQyahIKMdL8DrdAwhG9bMcD3NXuu-c94g');
-	var registrationIds = [];
-	message.addData('message',"Push message test....");
-	message.addData('title','Dr. Appointment' );
-	//message.addData('msgcnt','1'); // Shows up in the notification in the status bar
-	message.addData('soundname','beep.wav'); //Sound to play upon notification receipt - put in the www folder in app
-	//message.collapseKey = 'demo';
-	//message.delayWhileIdle = true; //Default is false
-	message.timeToLive = 3000;// Duration in seconds to hold in GCM and retry before timing out. Default 4 weeks (2,419,200 seconds) if not specified.
-
-	// At least one reg id required
-	registrationIds.push('APA91bF-wA3Zz0X3VKx7nm0NniG52MeSrOBVU8jmGSu2N1tdy4iAHqmGWNRK7KdRHcRMF4luWBb-47W4AVnDZBAhTe_udoVR7Q7Wl2LberWR2n9YZc7dvcl4_Y9XRWSp6R2qSMk9qvpTwqU8HPzaXa966OrFGJ7hFQ');
-
-	/**
-	* Parameters: message-literal, registrationIds-array, No. of retries, callback-function
-	*/
-	sender.send(message, registrationIds, 0, function (result) {
-		console.log(result);
-	});
-}
+//PUSH notification
 app.get('/push', function(req, res){
-	AndroidPush();
+	PushService({
+		platform: 'Android',
+		deviceID: 'APA91bF-wA3Zz0X3VKx7nm0NniG52MeSrOBVU8jmGSu2N1tdy4iAHqmGWNRK7KdRHcRMF4luWBb-47W4AVnDZBAhTe_udoVR7Q7Wl2LberWR2n9YZc7dvcl4_Y9XRWSp6R2qSMk9qvpTwqU8HPzaXa966OrFGJ7hFQ',
+		msgcount: 1,
+		message: 'Push message test....'
+	});
 	res.send('SUCCESS');
 });
 
---
+++ pushService.js
@@ -0,0 +1,42 @@
+function PushService(params){
+	var push = {
+		platform : params.platform,
+		deviceID : params.deviceID,
+		msgcount : params.msgcount,
+		message : params.message 
+	};
+	
+	if ( push.platform === 'Android' ) {
+		var config = {
+			sender: 'AIzaSyCQyahIKMdL8DrdAwhG9bMcD3NXuu-c94g',
+			title: 'Dr. Appointment',
+			sound: 'beep.wav',	//Sound to play upon notification receipt - put in the www folder in app
+			timeToLive: 3000,	//Duration in seconds to hold in GCM and retry before timing out. Default 4 weeks (2,419,200 seconds) if not specified.
+			delay : true,		//Default is false
+			retries: 1			//No. of retries
+		};
+
+		var gcm = require('node-gcm');
+		//API Server Key
+		var sender = new gcm.Sender(config.sender);
+		var registrationIds = [];
+		
+		//GCM Message
+		var message = new gcm.Message();
+		message.addData('title', config.title );
+		message.addData('soundname',config.sound); 
+		message.timeToLive = config.timeToLive; 
+		//message.delayWhileIdle = config.delay; 
+		message.addData('message', push.message);
+		//message.addData('msgcnt',push.msgcount); 
+		//message.collapseKey = 'demo';
+
+		// At least one reg id required
+		registrationIds.push(push.device);
+
+		//Parameters: message-literal, registrationIds-array, No. of retries, callback-function
+		sender.send(message, registrationIds, config.retries, function (result) {
+			console.log(result);
+		});
+	}
+}
